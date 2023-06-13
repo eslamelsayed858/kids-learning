e8767c41a10f4517f749d95473ab0c10d2e50a08
eslamelsayed858 <ee9122758@gmail.com>
2023-06-14 01:03:06 +0300

    up date

--- lib/constant/video_home.dart
+++ lib/constant/video_home.dart
@@ -8,7 +8,7 @@ class DataVideo {
   static const String image1 =
       "https://www.sayidaty.net/sites/default/files/styles/900_scale/public/2022-10/170815.jpeg";
   static const String image2 =
-      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.englishmtw.com%2F2021%2F03%2FEnglish-for-children.html&psig=AOvVaw0aJu2oe8kvtUBYHYp6PpBM&ust=1686174800726000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMj6uOTQr_8CFQAAAAAdAAAAABAE";
+      "https://i.ytimg.com/vi/H1rJc-gzKLg/maxresdefault.jpg";
 
   /// Link
   static const String link1 = "https://youtu.be/FP1It5kegv4";
